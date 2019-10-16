a735077b3a6aeafa612e13c42f8fd2fccf2ee944
Krzysztof Jamroz <jamro@jamro.net>
2019-10-16 22:24:41 +0200

    add grafana and influx

---
+++ influxdb/build.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os
+import docker
+
+imageTag = 'oversort/influxdb'
+
+client = docker.from_env()
+currentDir = os.path.dirname(os.path.realpath(__file__));
+
+print('Building docker image: ' + imageTag + '...')
+client.images.build(path=currentDir, tag=imageTag);
+print('Pushing docker image: ' + imageTag + '...')
+client.images.push(imageTag);
+print('DONE')
